920a2d6eb1080137f2fa9e4e0be29fb9efb01d12
tien.xuan.vo <tien.xuan.vo@gmail.com>
2020-07-08 09:00:40 +0700

    Fix test case

--- tests/unit/Extension/PhpPactTest.php
+++ tests/unit/Extension/PhpPactTest.php
@@ -34,17 +34,23 @@ class PhpPactTest extends Unit
     {
         $extension = new PhpPact([
             'env' => [
+                'PACT_MOCK_SERVER_HOST' => 'localhost',
+                'PACT_MOCK_SERVER_PORT' => 7200,
                 'PACT_CONSUMER_NAME' => 'ExampleOne',
                 'PACT_PROVIDER_NAME' => 'ExampleAPI',
-                'PACT_OUTPUT_DIR' => '/tmp',
+                'PACT_OUTPUT_DIR' => '/tmp/pact',
+                'PACT_LOG' => '/tmp/pact_log',
                 'PACT_CONSUMER_VERSION' => '1.0.0',
                 'PACT_CONSUMER_TAG' => 'master',
             ]
         ], []);
         $extension->initSuite($this->event);
+        $this->assertEquals('localhost', \getenv('PACT_MOCK_SERVER_HOST'));
+        $this->assertEquals('7200', \getenv('PACT_MOCK_SERVER_PORT'));
         $this->assertEquals('ExampleOne', \getenv('PACT_CONSUMER_NAME'));
         $this->assertEquals('ExampleAPI', \getenv('PACT_PROVIDER_NAME'));
-        $this->assertEquals('/tmp', \getenv('PACT_OUTPUT_DIR'));
+        $this->assertEquals('/tmp/pact', \getenv('PACT_OUTPUT_DIR'));
+        $this->assertEquals('/tmp/pact_log', \getenv('PACT_LOG'));
         $this->assertEquals('1.0.0', \getenv('PACT_CONSUMER_VERSION'));
         $this->assertEquals('master', \getenv('PACT_CONSUMER_TAG'));
 
@@ -64,6 +70,9 @@ class PhpPactTest extends Unit
         } catch (ProcessException $exception) {
             $this->fail('Pact server must be stopped successfully after suite');
         }
+
+        $this->assertEquals('./tests/_output/pact', \getenv('PACT_OUTPUT_DIR'));
+        $this->assertEquals('./tests/_output/pact_log', \getenv('PACT_LOG'));
 
         $server = $this->getProperty($extension, 'server');
         $this->assertInstanceOf(MockServer::class, $server);
